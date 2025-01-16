8c57c53669aba7a36a781399b90199bba2926b54
Abdulbarikassim <abdicsxhaka10@gmail.com>
2025-01-16 21:42:30 +0400

    feat: Implement user signup functionality
    - Added user sign controller to handle use registration.
    - Validates input fields to ensure all fields are provided.
    - checks the user with provided emails if they exits.
    - create a hashpassword to secure the storage.
    - Also created a verification token for email verification.
    - create the object user with input fields and save it in the data base.
    - generate an jwt token and set it as a cookie.
    
    The sign func

--- backEnd/controllers/auth.controllers.js
+++ backEnd/controllers/auth.controllers.js
@@ -1,6 +1,54 @@
+import { User } from "../models/user.models.js";
+import bcryptjs from "bcryptjs";
+import { generateTokenandSetCookie } from "../utils/generateTokenandSetCookie.js";
 export const signup = async (req, res) => {
-  res.send("sign up page");
+  const { email, password, name } = req.body;
+
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields should be available.");
+    }
+
+    const userAlreadyExits = await User.findOne({ email });
+
+    if (userAlreadyExits) {
+      res.status(400).json({ success: false, message: "user Already exits " });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    const verificationToken = Math.floor(
+      10000 + Math.random() * 900000
+    ).toString();
+    //create the user.
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
+      isVerified: false,
+      lastLogin: Date.now(),
+    });
+
+    await user.save();
+
+    generateTokenandSetCookie(res, user._id);
+
+    res.status(201).json({
+      success: true,
+      message: "User created Successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({ success: false, message: err.message });
+  }
 };
+
 export const signin = async (req, res) => {
   res.send("sign in page");
 };
