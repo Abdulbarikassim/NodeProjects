d517f0505279ae2cb0d06c642c0360b3e8078ca0
Abdulbarikassim <abdicsxhaka10@gmail.com>
2025-01-16 21:50:55 +0400

    Feat: separeted the routes functionality.
    created three routes for signin, signup and logout, also separating the
    functionality in a different folder to make the code clean.
    - the functionality is defined in the controller for each route.

--- backEnd/routes/auth.routes.js
+++ backEnd/routes/auth.routes.js
@@ -2,8 +2,8 @@ import express from "express";
 import { signin, signup, logout } from "../controllers/auth.controllers.js";
 const router = express.Router();
 
-router.get("/signup", signup);
-router.get("/signin", signin);
-router.get("/logout", logout);
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.post("/logout", logout);
 
 export default router;
